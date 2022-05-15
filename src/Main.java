68067e5ffb03015d9d294bf0fbc157382eb352d2
SergiK3 <sergikotrikadze@itvet.ge>
2022-05-16 01:08:36 +0400

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        EloCalculator eloCalculator = new EloCalculator();
+
+        System.out.println("Input number of Games below");
+        eloCalculator.setNumberOfGames(scanner.nextInt());
+        System.out.println("Input Winrate below");
+        eloCalculator.setWinratePercentage(scanner.nextDouble());
+        System.out.println("Input lp gain per game below");
+        eloCalculator.setLpPerGame(scanner.nextInt());
+
+        System.out.println("net gain in lp - " + eloCalculator.lpNet());
+        System.out.println("how many divisions - " + eloCalculator.lpIntoDivision());
     }
 }
